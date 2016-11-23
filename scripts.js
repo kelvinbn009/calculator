0287c32d00a454f4e93d46caa2b04befd080c257
Kelvin Bunei <keighb@live.com>
2016-11-23 23:46:39 +0300

    added subtraction function

---
+++ scripts.js
@@ -0,0 +1,23 @@
+var add= function(number1,number2){
+return number1+number2;
+}
+var number1=parseInt(prompt("supply a number"));
+var number2=parseInt(prompt("supply another number"));
+alert(add(number1,number2));
+//document.write(add(10,5));
+//document.write(result);
+var minus=function(number1,number2){
+return number1-number2};
+var number1=parseInt(prompt("supply a number"));
+var number2=parseInt(prompt("supply another number"));
+alert(minus(number1,number2));
+var multiply=function(number1,number2){return number1*number2};
+var number1=parseInt(prompt("supply a number"));
+var number2=parseInt(prompt("supply another number"));
+alert(multiply(number1,number2));
+var division=function(number1,number2){return number1/number2};
+var number1=parseInt(prompt("supply a number"));
+var number2=parseInt(prompt("supply another number"));
+alert(division(number1,number2));
+confirm("are you ready")
+
